5f19920b22100067502a737da5b531b8d39bab6d
jan <jan.messing@arcor.de>
2017-07-03 22:55:36 +0200

    esp8266 heartrateTest

--- Other/Arduino/esp8266_wearable/esp8266_wearable.ino
+++ Other/Arduino/esp8266_wearable/esp8266_wearable.ino
@@ -1,17 +1,35 @@
 #include <ESP8266WiFi.h>
 #include <b64.h>
 #include <PubSubClient.h>
+#define pulsePin 0
 
-int PulseSensorPurplePin = 0;        // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
-int LED = 16;   //  The on-board Arduion LED
-int Signal;                // holds the incoming raw data. Signal value can range from 0-1024
-int Threshold = 550;            // Determine which Signal to "count as a beat", and which to ingore. 
+//int pulsePin = 0;      // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
+int LED = 16;          //  The on-board Arduion LED
+int Signal;            // holds the incoming raw data. Signal value can range from 0-1024
+int Threshold = 550;   // Determine which Signal to "count as a beat", and which to ingore. 
 String msgString;
 
+//  VARIABLES show BPM
+int rate[10];                    
+unsigned long sampleCounter = 0; 
+unsigned long lastBeatTime = 0;  
+unsigned long lastTime = 0, N;
+int BPM = 0;
+int IBI = 0;
+int P = 512;
+int T = 512;
+// int Threshold = 512;  
+int amp = 100;                   
+int PulseData;
+boolean Pulse = false;
+boolean firstBeat = true;          
+boolean secondBeat = true;
+boolean QS = false;   
+
 // Update these with values suitable for your network.
-const char* ssid = "WiFi";
-const char* password = "PASSWORD";
-const char* mqtt_server = "ipconfig"; // http://www.hivemq.com/demos/websocket-client/ 212.72.72.12
+const char* ssid = "Jan Messing";
+const char* password = "castle-multi-pecos-vaduz";
+const char* mqtt_server = "172.20.10.9"; // http://www.hivemq.com/demos/websocket-client/ 212.72.72.12
 
 
 WiFiClient espClient;
@@ -107,13 +125,13 @@ void loop() {
     lastMsg = now;
     ++value;
     msgString = String(Signal);
-    snprintf (msg, 75, "#%ld", Signal);
+    snprintf (msg, 75, "Heartrate: #%ld", Signal);
     Serial.print("Publish message: ");
     Serial.println(msg);
     client.publish("p6/mqtt", msg);
   }
 
-  Signal = analogRead(PulseSensorPurplePin);  // Read the PulseSensor's value. 
+  Signal = analogRead(pulsePin);  // Read the PulseSensor's value. 
                                               // Assign this value to the "Signal" variable.
    Serial.println(Signal);                    // Send the Signal value to Serial Plotter.
    
@@ -124,6 +142,90 @@ void loop() {
    }
 delay(10);
 
+  // Loop BPM
+  if (QS == true) {
+                Serial.println("BPM: "+ String(BPM));
+                QS = false;
+              } else if (millis() >= (lastTime + 2)) {
+                readPulse(); 
+                lastTime = millis();
+              }       
 
   
+} // Ende loop
+
+void readPulse() {
+
+  PulseData = analogRead(pulsePin);              
+  sampleCounter += 2;                           
+  int N = sampleCounter - lastBeatTime;   
+
+  detectSetHighLow();
+
+  if (N > 250) {  
+    if ( (PulseData > Threshold) && (Pulse == false) && (N > (IBI / 5) * 3) )
+      pulseDetected();
+  }
+
+  if (PulseData < Threshold && Pulse == true) {  
+    Pulse = false;
+    amp = P - T;
+    Threshold = amp / 2 + T;  
+    P = Threshold;
+    T = Threshold;
+  }
+
+  if (N > 2500) {
+    Threshold = 512;
+    P = 512;
+    T = 512;
+    lastBeatTime = sampleCounter;
+    firstBeat = true;            
+    secondBeat = true;           
+  }
+
+}
+
+void detectSetHighLow() {
+
+  if (PulseData < Threshold && N > (IBI / 5) * 3) {
+    if (PulseData < T) {                       
+      T = PulseData;                         
+    }
+  }
+
+  if (PulseData > Threshold && PulseData > P) {    
+    P = PulseData;                           
+  }                                       
+
+}
+
+void pulseDetected() {
+  Pulse = true;                           
+  IBI = sampleCounter - lastBeatTime;     
+  lastBeatTime = sampleCounter;           
+
+  if (firstBeat) {                       
+    firstBeat = false;                 
+    return;                            
+  }
+  if (secondBeat) {                    
+    secondBeat = false;                
+    for (int i = 0; i <= 9; i++) {   
+      rate[i] = IBI;
+    }
+  }
+
+  word runningTotal = 0;                   
+
+  for (int i = 0; i <= 8; i++) {          
+    rate[i] = rate[i + 1];            
+    runningTotal += rate[i];          
+  }
+
+  rate[9] = IBI;                      
+  runningTotal += rate[9];            
+  runningTotal /= 10;                 
+  BPM = 60000 / runningTotal;         
+  QS = true;                              
 }
