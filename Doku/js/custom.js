8188df88dcac6e34b55a58f735c848bad12e8389
Sergej Nawalnew <hello@nawalnew.com>
2017-07-04 20:55:40 +0200

    doku upload

---
+++ Doku/js/custom.js
@@ -0,0 +1,361 @@
+// $(document).ready(function() {
+//
+// 	$("body").css("display", "none");
+//
+//     $("body").fadeIn(1000);
+//
+// 	$("a.transition").click(function(event){
+// 		event.preventDefault();
+// 		linkLocation = this.href;
+// 		$("body").fadeOut(1000, redirectPage);
+// 	});
+//
+// 	function redirectPage() {
+// 		window.location = linkLocation;
+// 	}
+//
+// });
+//
+//
+//
+// $(window).load(function() {
+//   $("html, body").animate({ scrollTop: $(document).height() }, 2000);
+// });
+
+/* CLICK ACTIONS */
+
+$( document ).ready(function() {
+    $(".leaf").click(function(){
+            $(this).addClass("active");
+    });
+});
+
+
+// WURZELN
+
+$("#wurzel01").animatedModal({
+	modalTarget:'animatedModal-w1',
+        animatedIn:'zoomIn',
+        animatedOut:'zoomOut',
+        color:'#FFFFFF',
+				overflow: 'scroll',
+				beforeOpen: function() {
+
+
+
+
+				},
+				afterClose: function() {
+
+
+				}
+
+    });
+
+    $("#wurzel02").animatedModal({
+    	modalTarget:'animatedModal-w2',
+            animatedIn:'zoomIn',
+            animatedOut:'zoomOut',
+            color:'#FFFFFF',
+    				overflow: 'scroll',
+    				beforeOpen: function() {
+
+
+
+
+    				},
+    				afterClose: function() {
+
+
+    				}
+
+        });
+
+        $("#wurzel03").animatedModal({
+        	modalTarget:'animatedModal-w3',
+                animatedIn:'zoomIn',
+                animatedOut:'zoomOut',
+                color:'#FFFFFF',
+        				overflow: 'scroll',
+        				beforeOpen: function() {
+
+
+
+
+        				},
+        				afterClose: function() {
+
+
+        				}
+
+            });
+
+            $("#wurzel04").animatedModal({
+            	modalTarget:'animatedModal-w4',
+                    animatedIn:'zoomIn',
+                    animatedOut:'zoomOut',
+                    color:'#FFFFFF',
+            				overflow: 'scroll',
+            				beforeOpen: function() {
+
+
+
+
+            				},
+            				afterClose: function() {
+
+
+            				}
+
+                });
+
+                $("#wurzel05").animatedModal({
+                	modalTarget:'animatedModal-w5',
+                        animatedIn:'zoomIn',
+                        animatedOut:'zoomOut',
+                        color:'#FFFFFF',
+                				overflow: 'scroll',
+                				beforeOpen: function() {
+
+
+
+
+                				},
+                				afterClose: function() {
+
+
+                				}
+
+                    });
+
+// BLÄTTER
+$("#leaf01").animatedModal({
+	modalTarget:'animatedModal',
+        animatedIn:'zoomIn',
+        animatedOut:'zoomOut',
+        color:'#FFFFFF',
+				overflow: 'scroll',
+				beforeOpen: function() {
+
+
+
+
+				},
+				afterClose: function() {
+
+
+				}
+
+    });
+
+
+
+		$("#leaf02").animatedModal({
+			modalTarget:'animatedModal-2',
+		        animatedIn:'zoomIn',
+		        animatedOut:'zoomOut',
+		        color:'#FFFFFF',
+						overflow: 'scroll',
+						beforeOpen: function() {
+
+
+
+
+						},
+						afterClose: function() {
+
+
+						}
+
+		    });
+
+
+				$("#leaf03").animatedModal({
+					modalTarget:'animatedModal-3',
+				        animatedIn:'zoomIn',
+				        animatedOut:'zoomOut',
+				        color:'#FFFFFF',
+								overflow: 'scroll',
+								beforeOpen: function() {
+
+
+
+
+								},
+								afterClose: function() {
+
+
+								}
+
+				    });
+
+            $("#leaf04").animatedModal({
+    					modalTarget:'animatedModal-4',
+    				        animatedIn:'zoomIn',
+    				        animatedOut:'zoomOut',
+    				        color:'#FFFFFF',
+    								overflow: 'scroll',
+    								beforeOpen: function() {
+
+
+
+
+    								},
+    								afterClose: function() {
+
+
+    								}
+
+    				    });
+
+                $("#leaf05").animatedModal({
+        					modalTarget:'animatedModal-5',
+        				        animatedIn:'zoomIn',
+        				        animatedOut:'zoomOut',
+        				        color:'#FFFFFF',
+        								overflow: 'scroll',
+        								beforeOpen: function() {
+
+
+
+
+        								},
+        								afterClose: function() {
+
+
+        								}
+
+        				    });
+
+                    $("#leaf06").animatedModal({
+            					modalTarget:'animatedModal-6',
+            				        animatedIn:'zoomIn',
+            				        animatedOut:'zoomOut',
+            				        color:'#FFFFFF',
+            								overflow: 'scroll',
+            								beforeOpen: function() {
+
+
+
+
+            								},
+            								afterClose: function() {
+
+
+            								}
+
+            				    });
+
+                        $("#leaf07").animatedModal({
+                					modalTarget:'animatedModal-7',
+                				        animatedIn:'zoomIn',
+                				        animatedOut:'zoomOut',
+                				        color:'#FFFFFF',
+                								overflow: 'scroll',
+                								beforeOpen: function() {
+
+
+
+
+                								},
+                								afterClose: function() {
+
+
+                								}
+
+                				    });
+
+                            $("#leaf08").animatedModal({
+                    					modalTarget:'animatedModal-8',
+                    				        animatedIn:'zoomIn',
+                    				        animatedOut:'zoomOut',
+                    				        color:'#FFFFFF',
+                    								overflow: 'scroll',
+                    								beforeOpen: function() {
+
+
+
+
+                    								},
+                    								afterClose: function() {
+
+
+                    								}
+
+                    				    });
+
+                                $("#leaf09").animatedModal({
+                        					modalTarget:'animatedModal-9',
+                        				        animatedIn:'zoomIn',
+                        				        animatedOut:'zoomOut',
+                        				        color:'#FFFFFF',
+                        								overflow: 'scroll',
+                        								beforeOpen: function() {
+
+
+
+
+                        								},
+                        								afterClose: function() {
+
+
+                        								}
+
+                        				    });
+
+                                    $("#leaf10").animatedModal({
+                            					modalTarget:'animatedModal-10',
+                            				        animatedIn:'zoomIn',
+                            				        animatedOut:'zoomOut',
+                            				        color:'#FFFFFF',
+                            								overflow: 'scroll',
+                            								beforeOpen: function() {
+
+
+
+
+                            								},
+                            								afterClose: function() {
+
+
+                            								}
+
+                            				    });
+
+                                        $("#leaf11").animatedModal({
+                                					modalTarget:'animatedModal-11',
+                                				        animatedIn:'zoomIn',
+                                				        animatedOut:'zoomOut',
+                                				        color:'#FFFFFF',
+                                								overflow: 'scroll',
+                                								beforeOpen: function() {
+
+
+
+
+                                								},
+                                								afterClose: function() {
+
+
+                                								}
+
+                                				    });
+
+                                            $("#leaf12").animatedModal({
+                                    					modalTarget:'animatedModal-12',
+                                    				        animatedIn:'zoomIn',
+                                    				        animatedOut:'zoomOut',
+                                    				        color:'#FFFFFF',
+                                    								overflow: 'scroll',
+                                    								beforeOpen: function() {
+
+
+
+
+                                    								},
+                                    								afterClose: function() {
+
+
+                                    								}
+
+                                    				    });
